0f97f0faaf643ee3649b4651dcd5e94180cbc1e8
Syed Shibli <syedsks@x.Optilink>
2023-12-27 16:10:28 +0530

    create erd

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_09_131350) do
-  create_table "admins", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "login"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2023_09_27_070234) do
   create_table "roles", force: :cascade do |t|
     t.string "name"
     t.string "grade"
